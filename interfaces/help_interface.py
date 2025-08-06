88f6619f595e1917907b78024df76dd4f242b4ec
骑士姬 <2067834160@qq.com>
2025-08-06 15:27:34 +0800

    feat: 增加帮助页面

---
+++ interfaces/help_interface.py
@@ -0,0 +1,39 @@
+# interfaces/help_interface.py
+from PySide6.QtCore import Qt
+from typing import Self, Optional
+from qfluentwidgets import HyperlinkButton
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
+
+from styles.default import TITLE_STYLE
+
+class HelpInterface(QWidget):
+    def __init__(self: Self, parent: Optional[QWidget] = None) -> None:
+        super().__init__(parent=parent)
+        # 设置对象名
+        self.setObjectName("HelpInterface")
+        # 初始化 UI
+        self.init_ui()
+    
+    def init_ui(self: Self) -> None:
+        layout: QVBoxLayout = QVBoxLayout(self)
+        layout.setAlignment(Qt.AlignCenter) # pyright: ignore[reportAttributeAccessIssue]
+        # 标题
+        title: QLabel = QLabel("帮助与支持", self)
+        title.setStyleSheet(TITLE_STYLE)
+        # 仓库链接跳转按钮
+        gitee_btn: HyperlinkButton = HyperlinkButton(
+            "https://gitee.com/qishiji/Ouroboros", 
+            "gitee 仓库", 
+            self,
+        )
+        github_btn: HyperlinkButton = HyperlinkButton(
+            "https://github.com/knightfemale/Ouroboros", 
+            "GitHub 仓库", 
+            self,
+        )
+        # 加入到布局
+        layout.addWidget(title)
+        layout.addSpacing(20)
+        layout.addWidget(gitee_btn)
+        layout.addWidget(github_btn)
+        self.setLayout(layout)
